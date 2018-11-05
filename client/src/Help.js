e44d2ac16d16baa358b2c8f429c3a48ec9379acc
Tomás Di Domenico <tdido@tdido.com.ar>
2018-11-05 16:15:32 +0100

    Implemented changes to Help section.

--- client/src/Help.js
+++ client/src/Help.js
@@ -8,7 +8,7 @@ import table from './img/ranking-summary-table.png';
 import Divider from'@material-ui/core/Divider';
 
 
-const baseUrl = 'http://api.vulcanspot.org'
+const baseUrl = 'http://vulcanspot.org/api'
 const styles = theme => ({
     root: {
         display: 'flex',
@@ -30,7 +30,7 @@ class Help extends React.Component {
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                     <Typography component='p' variant='h4' style={{textAlign:'center'}}>
-                        Vulcanspot Help 
+                        vulcanSpot Help 
                     </Typography>
                     <br/ >
                     <br/ >
@@ -51,14 +51,14 @@ class Help extends React.Component {
 </li>
 </ol>
 <h2><a id="1_FAQs_16"></a>1. FAQs</h2>
-<p><strong>1.1 What is VulcanSpot?</strong></p>
-<p>VulcanSpot is a web tool that integrates genome-wide information provided by large-scale screening experiments to detect Genetic Vulnerabilities in Cancer.</p>
+<p><strong>1.1 What is vulcanSpot?</strong></p>
+<p>vulcanSpot is a web tool that integrates genome-wide information provided by large-scale screening experiments to detect Genetic Vulnerabilities in Cancer.</p>
 <p>The method proposes prioritized drugs to target these cancer-specific gene dependencies using a weighted scoring system based on well known drug-gene relationships and drug repositioning strategies.</p>
 <p><strong>1.2. How do I cite VulscanSpot?</strong></p>
 <p>When citing our application, please refer to our publication:<br/>
 [PMID and link]</p>
-<p><strong>1.3 May I use VulcanSpot for commercial purposes?</strong></p>
-<p>VulcanSpot is intended for research purposes and academic use exclusively.<br/>
+<p><strong>1.3 May I use vulcanSpot for commercial purposes?</strong></p>
+<p>vulcanSpot is intended for research purposes and academic use exclusively.<br/>
 If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour (<a href="mailto:falshahrour@cnio.es">falshahrour@cnio.es</a>).</p>
 <h2><a id="2_Main_Query_Optionsa_namequeryoptionsa_35"></a>2. Main Query Options<a name="query-options"></a></h2>
 <p>In order to query the database and obtain a <strong>list of genetic alterations associated to therapeutic vulnerabilities</strong>, users must provide the HUGO Gene Symbols of a gene or genes with a matched dependency in the database. Input genes are defined as <strong>Gene(s) A</strong> and correspond to candidate genes with a genetic alteration.</p>
@@ -72,7 +72,11 @@ If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour
 <li><strong>GDs on cancer genes</strong>: Select this option in case you are interested in therapeutic vulnerabilites related to Cancer Genes only.</li>
 </ul>
 <h2><a id="4_Output_Summary_Tablea_nameoutputsummarytablea_51"></a>4. Output Summary Table<a name="output-summary-table"></a></h2>
-<p>VulcanSpot main output consists on a <strong>prioritized list of drugs targeting GDs</strong> detected in the user's gene list. By default VulcanSpot shows the top-10 drug-GDs relationships together with gene and drug information and functional annotation. Users can expand the main output and download the full results table.</p>
+<p>
+
+vulcanSpot's main output consists on a <strong>prioritized list of drugs targeting GDs</strong> detected in the user's gene list (FDR&lt;0.25). By default vulcanSpot shows the top-10 drug-GDs relationships together with gene and drug information and functional annotation. Users can expand the final output and download the full results table.
+
+</p>
 <p>The output is divided in the main three sections described below:</p>
 <ul>
 <li><strong>Genetic alteration</strong>: Genotype-Context alterations for which gene dependencies were found.
@@ -104,12 +108,15 @@ If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour
 <li><strong>Therapies</strong>: Computed drug prescriptions amongst the identified dependencies.
 <ul>
 <li>Drug: Pharmacological common name.</li>
-<li>Drug score: VulcanSpot estimates the drug response and treatment suitability using the Drug Score (DScore) calculated by PanDrugs (2018.04.30) for the Gene B - Drug interaction. The score ranges between 0 an 1, where the higher the score, the better is the drug-gene association. For more information about how this score was obtained, check PanDrugs help section: <a href="https://www.pandrugs.org/#!/help">https://www.pandrugs.org/#!/help</a>.</li>
+<li>Drug score: vulcanSpot estimates the drug response and treatment suitability using the Drug Score (DScore) calculated by PanDrugs (2018.04.30) for the Gene B - Drug interaction. The score ranges between 0 an 1, where the higher the score, the better is the drug-gene association. For more information about how this score was obtained, check PanDrugs help section: <a href="https://www.pandrugs.org/#!/help">https://www.pandrugs.org/#!/help</a>.</li>
 <li>LINCS score: also known as KDCP score. Measures the similarity between each pair of gene expression signatures derived from gene knock-down (KD) experiments and compound experiments (CP) from the CMap-L1000 project, corrected by the distance between the drug and its target. The score ranges between 0 an 1, where the higher the score, the better is the association.</li>
 </ul>
 </li>
 </ul>
-<p>VulcanSpot main output offers a ranking of prioritized drugs to target statistically significant GDs (FDR&lt;0.25). The ranking score is ordered <em>decreasingly</em> following the rational criteria:<br/>
+<p>
+vulcanSpot's final ranking of prioritized drugs is ordered *decreasingly* according to the following rationale:
+        
+        <br/>
 <center><img alt="" width="500" height="583" src={table} /></center></p>
 <ol>
 <li>Both gene A and gene B are druggable and meet DScore ≥ 0.6 and KDCP score ≥ 0.9.</li>
@@ -119,7 +126,7 @@ If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour
 </ol>
 <h2><a id="api"></a>5. API reference</h2>
                         <Typography component='p' variant='body1'>
-                            VulcanSpot offers programmatic access to its data through a RESTful API.
+                            vulcanSpot offers programmatic access to its data through a RESTful API.
                             This is a list of available REST endpoints:
                         </Typography>
                         <br/>
@@ -128,7 +135,7 @@ If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour
                             {baseUrl}/genes 
                         </Typography>
                         <Typography component='p' variant='body1'>
-                            Lists all genes in VulcanSpot
+                            Lists all genes in vulcanSpot
                         </Typography>
 
                         <Divider className={classes.divider} />
@@ -136,21 +143,21 @@ If you are interested in a collaboration, please contact Dr. Fátima Al-Shahrour
                             {baseUrl}/genes/{'{gene symbol}'} 
                         </Typography>
                         <Typography component='p' variant='body1'>
-                            Lists details for a gene in VulcanSpot
+                            Lists details for a gene in vulcanSpot
                         </Typography>
                         <Divider className={classes.divider} />
                         <Typography component='p' variant='h6'>
                             {baseUrl}/genes/{'{gene symbol}'}/treatments
                         </Typography>
                         <Typography component='p' variant='body1'>
-                            Lists available gene dependencies and treatments for a gene in VulcanSpot
+                            Lists available gene dependencies and treatments for a gene in vulcanSpot
                         </Typography>
                         <Divider className={classes.divider} />
                         <Typography component='p' variant='h6'>
                             {baseUrl}/contexts 
                         </Typography>
                         <Typography component='p' variant='body1'>
-                            Lists all cancer contexts in VulcanSpot
+                            Lists all cancer contexts in vulcanSpot
                         </Typography>
 
                     </Paper>
